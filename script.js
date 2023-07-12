85eee31fd10f4b7b2c41fd08c1112a9903d9eed8
Igor De Nadai <igoroliveiradenadai@gmail.com>
2023-07-11 21:34:23 -0300

    Async function
    Now the request to the API usa a async and await function.

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const APIkey = "6JeMl3RWmrTX9j1gTh5Mh0tWb8ZtGcOiisOWFTDH"
-const APIurl = `https://api.nasa.gov/planetary/apod?api_key=${APIkey}` 
+const APIurl = `https://api.nasa.gov/planetary/apod?api_key=${APIkey}`
 
-const requisition = fetch(APIurl)
-    .then(function(res){
-        return res.json()
-    })
-    .then(function(resAsJson){ 
-        changeImage(resAsJson.url,resAsJson.title,resAsJson.explanation,resAsJson.media_type)
-    })
+async function getResponse() {
+    const response = await fetch(APIurl);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const resAsJson = await response.json();
+    changeImage(resAsJson.url, resAsJson.title, resAsJson.explanation, resAsJson.media_type)
+}
 
 
-function changeImage(contentURL,title,description,media_type){
+function changeImage(contentURL, title, description, media_type) {
     let type = media_type
-    
-    if(type != "video"){
+
+    if (type != "video") {
         let img = document.createElement("img")
         img.id = "image"
         img.src = contentURL
@@ -21,9 +24,9 @@ function changeImage(contentURL,title,description,media_type){
         img.style.maxWidth = "100%";
         img.style.height = "auto";
         document.getElementById("article_img").innerHTML = "";
-        document.getElementById("article_img").appendChild(img)    
-         
-    }else{
+        document.getElementById("article_img").appendChild(img)
+
+    } else {
         let video = document.createElement("iframe")
         video.id = "video"
         video.src = contentURL
@@ -31,7 +34,9 @@ function changeImage(contentURL,title,description,media_type){
         document.getElementById("article_video").innerHTML = "";
         document.getElementById("article_video").appendChild(video)
     }
-    
+
     document.getElementById("imageTitle").innerHTML = `The image of the day is: ${title}`
     document.getElementById("description").innerHTML = description
 }
+
+getResponse()
