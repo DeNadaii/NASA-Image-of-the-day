bdfb815f70a9cbab445d7eea3843d123ae75f647
Igor De Nadai <78096961+DeNadaii@users.noreply.github.com>
2023-12-08 15:36:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function getResponse() {
     }
 
     const resAsJson = await response.json();
-    console.log(response.json())
+    console.log(resAsJson)
     changeImage(resAsJson.url, resAsJson.title, resAsJson.explanation, resAsJson.media_type)
 }
 
